08820cf016415d26790c824b3fa42fe780352082
shoaibmhr <shoaibmhr895@gmail.com>
2026-08-25 11:44:47 +0500

    chore: initialize React Vite Tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Admin Panel</h1>
+    </div>
+  );
+}
+
+export default App;
